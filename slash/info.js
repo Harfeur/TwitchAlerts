f731b65166ad37b2f083ee93489562721abf66c2
Harfeur <maxime.chourre@harfeur.fr>
2022-08-20 20:33:37 +0200

    Updated strings

--- slash/info.js
+++ slash/info.js
@@ -1,6 +1,15 @@
 const GeneralController = require("../controllers/generalController");
+const {languagesList, getString} = require("../modules/language");
 
 exports.run = GeneralController.info;
+
+let names = {}
+let descs = {}
+
+for (const [lang, _] of languagesList) {
+    names[lang] = getString(lang, "INFO_CMD_NAME");
+    descs[lang] = getString(lang, "INFO_CMD_DESC");
+}
 
 exports.commandData = {
     name: "info",
